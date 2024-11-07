447cfd7581e76bf95e9b08e21665e2814604246f
unknown <afindo.mi01@gmail.com>
2024-11-08 00:35:05 +0800

    Menghapus Catatan

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -3,29 +3,35 @@ const {
     getAllNotesHandler,
     getNoteByIdHandler,
     editNoteByIdHandler,
-  } = require('./handler');
-  
+    deleteNoteByIdHandler,
+} = require('./handler');
+
 const routes = [
     {
-      method: 'POST',
-      path: '/notes',
-      handler: addNoteHandler,
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
     },
     {
-      method: 'GET',
-      path: '/notes',
-      handler: getAllNotesHandler,
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
     },
     {
-      method: 'GET',
-      path: '/notes/{id}',
-      handler: getNoteByIdHandler,
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
     },
     {
         method: 'PUT',
         path: '/notes/{id}',
         handler: editNoteByIdHandler,
-      },
-  ];
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
 
 module.exports = routes;
